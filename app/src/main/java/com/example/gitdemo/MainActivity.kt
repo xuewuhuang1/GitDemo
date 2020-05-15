a31b9ff14e30b4741612f7f32c2b21ef17958de0
huangxuewu <2995657864@qq.com>
2020-05-15 16:53:44 +0800

    implement：button onClick Listener

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private var number = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text="$number"
+        button.setOnClickListener {
+            textView.text="${++number}"
+        }
     }
 }
